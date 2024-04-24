e7fde48c7bac4372ace0ed4f6a18d1943c68d3c9
tapeds <frrllmatthew@gmail.com>
2024-04-24 21:07:53 +0700

    feat: add providers

---
+++ src/app/providers.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryOptions,
+} from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+
+import api from '@/lib/api';
+
+const defaultQueryFn = async ({ queryKey }: QueryOptions) => {
+  const { data } = await api.get(`${queryKey?.[0]}`);
+  return data;
+};
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+});
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Toaster position='top-center' />
+      {children}
+    </QueryClientProvider>
+  );
+}
