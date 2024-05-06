6412529e986e11289834640600b3ba5cbceedcb2
Ainun Nadhifah Syamsiyah <58164571+ainunns@users.noreply.github.com>
2024-05-07 00:27:49 +0700

    feat: add 404 page (#6)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from 'next';
+import * as React from 'react';
+
+import NextImage from '@/components/NextImage';
+
+export const metadata: Metadata = {
+  title: '404',
+};
+
+export default function NotFound() {
+  return (
+    <main className='flex min-h-screen w-screen items-center justify-center bg-slate-200'>
+      <NextImage
+        src='/NotFound.png'
+        alt='404'
+        width={1920}
+        height={1080}
+        className='w-2/3 xl:w-1/2'
+      />
+    </main>
+  );
+}
